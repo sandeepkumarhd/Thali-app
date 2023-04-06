956e6ad191ec1a7cf9b0a45f41477e9982844b14
Sandeepkumar15267 <ksandeep15267@gmail.com>
2023-04-06 11:36:10 +0530

    responsive ui

--- src/Components/MainHeader/MainHeader.js
+++ src/Components/MainHeader/MainHeader.js
@@ -1,28 +1,46 @@
 import style from "./MainHeader.module.css";
-import Checkout from "../Checkout/Checkout";
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const MainHeader = () => {
+  const [isClicked, setIsClked] = useState(false);
+  const menuHandler = () => {
+    setIsClked((pre) => !pre);
+    console.log("jhsdcjfhjsd")
+  };
   return (
-    <header className={style.header}>
-      <nav>
-        <Link to={"/"}>
-          <h1>HDCB Restaurant</h1>
-        </Link>
+    <div>
+      <header className={style.header}>
+        <nav>
+          <Link to={"/"}>
+            <h1>HDCB</h1>
+          </Link>
 
-        <ul>
-          <NavLink to={"/product"}>
-            <li>Products</li>
-          </NavLink>
-          <NavLink to={"/thali"}>
-            <li>Your Thali</li>
-          </NavLink>
-          <NavLink to={"/bookedItem"}>
-            <li>Booked Thali</li>
-          </NavLink>
-        </ul>
-      </nav>
-    </header>
+          <ul className={""}>
+            <NavLink to={"/product"}>
+              <li>Products</li>
+            </NavLink>
+            <NavLink to={"/thali"}>
+              <li>Your Thali</li>
+            </NavLink>
+            <NavLink to={"/bookedItem"}>
+              <li>Booked Thali</li>
+            </NavLink>
+          </ul>
+          
+          <p><NavLink to={"/thali"}>
+              <li>Your Thali</li>
+            </NavLink></p>
+          <i
+            onClick={menuHandler}
+            className={`fa-solid fa-bars`}
+          ></i>
+        </nav>
+      </header>
+      <section className={!isClicked ? "section" : "sectionMenu"}>
+        
+      </section>
+    </div>
   );
 };
 export default MainHeader;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./Redux Store/Store";
 import { Provider } from "react-redux";
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
